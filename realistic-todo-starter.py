053a771c9404f6a9183a54b991b8fdd663dd55c4
white-prog <sabinhameed55@gmail.com>
2024-12-04 21:15:13 +0530

    Realistic todo starter with index least methods

--- realistic-todo-starter.py
+++ realistic-todo-starter.py
@@ -13,28 +13,43 @@ See README.md for full requirements
 class TodoList:
     def __init__(self):
         # Initialize your task storage here
-        pass
+        self.task = []
+
 
     def add_task(self, description: str) -> bool:
         """Add a new task"""
-        pass
+        self.task.append([description,False])
+        return True
+        
 
     def view_tasks(self) -> list:
         """Return all tasks"""
-        pass
+        return self.task
 
     def complete_task(self, task_id: int) -> bool:
         """Mark a task as complete"""
-        pass
+        self.task[task_id][1] = True
+        return True
+        
+        
 
     def delete_task(self, task_id: int) -> bool:
         """Delete a task"""
-        pass
+        self.task.remove(self.task[task_id])
+        return True
 
 
 def main():
     # Implement your command line interface here
-    pass
+    todo = TodoList()
+    todo.add_task("Play football")
+    todo.add_task("Study law")
+    todo.add_task("Hear music")
+
+    todo.delete_task(2)
+    todo.complete_task(0)
+    print(todo.view_tasks())
+
 
 
 if __name__ == "__main__":
